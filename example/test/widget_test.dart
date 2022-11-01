213559d95869ee5f978cac240db84e378b0c2a5a
Bart Ribbers <bart@iconica.nl>
2022-11-01 08:38:55 +0100

    Add BSD-3-Clause license

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -1,1 +1,3 @@
-
+// SPDX-FileCopyrightText: 2022 Iconica
+//
+// SPDX-License-Identifier: BSD-3-Clause
--- lib/single_character_input.dart
+++ lib/single_character_input.dart
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 Iconica
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
 library single_character_input;
 
 export 'src/input_character.dart';
